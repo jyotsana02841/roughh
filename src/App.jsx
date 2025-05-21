c2c1ddb261cbfca48875c36550ddd63461247b01
jyotsana02841 <joshikyotsana82@gmail.com>
2025-05-21 19:08:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div
+      onClick={() => setCount((count) => count + 1)}
+      className="card h-screen w-screen flex items-center justify-center bg-gradient-to-r from-rose-100 via-pink-200 to-amber-10"
+    >
+      <span className="font-bold text-pink-500  w-50 h-25 flex justify-center items-center bg-gradient-to-br from-pink-200 via-rose-100 to-fuchsia-200">Radha Radha: {count}</span>
+    </div>
+  );
+}
+
+export default App;
