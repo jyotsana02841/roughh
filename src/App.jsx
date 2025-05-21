002cbbae5f9d24b522e2bbc9aefcbc08d29126a4
jyotsana02841 <joshikyotsana82@gmail.com>
2025-05-21 19:10:25 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       onClick={() => setCount((count) => count + 1)}
       className="card h-screen w-screen flex items-center justify-center bg-gradient-to-r from-rose-100 via-pink-200 to-amber-10"
     >
-      <span className="font-bold text-pink-500  w-50 h-25 flex justify-center items-center bg-gradient-to-br from-pink-200 via-rose-100 to-fuchsia-200">Radha Radha: {count}</span>
+      <span className="font-bold text-pink-500  w-50 h-25 flex justify-center items-center bg-gradient-to-br from-pink-200 via-rose-100 to-fuchsia-200">Radhe Radhe: {count}</span>
     </div>
   );
 }
